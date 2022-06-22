69da22ca3e264cf2fe9c9b057209dbbd9c716504
kimyongho-1 <80937136+kimyongho-1@users.noreply.github.com>
2022-06-22 23:18:44 +0900

    전체수정
    전체수정

--- ThirdCam.cs
+++ ThirdCam.cs
@@ -5,57 +5,84 @@ using UnityEngine;
 public class ThirdCam : MonoBehaviour
 {
     public GameObject DD;
-    public Player _chr;
-    Vector3 pos;
-    Vector3 rot; float horMove, vertMove; bool _isLinearTr;
-     [SerializeField] float _obstacleDist = 0.9f;
-    float camDistance; Camera _cam;
-    [SerializeField] float _rotSpeed, _camSpeed, _maxDistance, _scrollSpeed;
-    [SerializeField] Vector2 _MouseSensitivity;
+    public Player _chr; // 캐릭터이자, 카메라가 바라볼 대상
+    Vector3 pos, _obs; // 이동할 목적지 & 레이대상
+    Vector3 rot;  // 회전할 목적지
+    bool _isLinearTr; // 보간할지, 대입할지
+    float camDistance; Camera _cam; // 현재 카메라거리 & 카메라오브젝트
+
+    // 카메라 회전,이동속도 , 대상까지의 최대거리 , 스크롤속도, 마우스 회전속도
+    [SerializeField]float _rotSpeed, _camSpeed, _maxDistance, _scrollSpeed, _mouseSensi;
+    
+    // 구형공간에서 사용할 반지름(camDistance)와 각도축(방위각 앙각)
+    float _radius, _azimuth, _elevation;
+    [SerializeField] Vector2 _elevation_Range;
+    #region GetSetter
+    public float Radius
+    {
+        get { return _radius; }
+        set { _radius = value; }
+    }
+    public float Azimuth
+    {
+        get { return _azimuth; }
+        set
+        {
+            _azimuth = Mathf.Clamp(value
+          , -360f * Mathf.Deg2Rad, 360f * Mathf.Deg2Rad);
+        }
+    }
+    public float Elevation
+    {
+        get { return _elevation; }
+        set
+        {
+            _elevation = Mathf.Clamp(value
+          , _elevation_Range.x * Mathf.Deg2Rad, _elevation_Range.y * Mathf.Deg2Rad);
+        }
+    }
+    #endregion
     private void Start()
     {
-        horMove = -90f* Mathf.Deg2Rad;
         _cam = transform.GetComponent<Camera>();
         _maxDistance = 7f;
         if (_chr == null)
         { _chr = GameObject.Find("@Player").GetComponent<Player>(); }
         camDistance = _maxDistance;
         pos = _chr.transform.position - transform.forward * camDistance;
-      //  StartCoroutine(draw());
+
+        GetSphereCoord(pos);
+      
     }
 
-    IEnumerator draw()
-    {
-        while (DD.transform.childCount > 0)
-        {
-            DD.transform.GetChild(0).position = 
-                new Vector3(0, transform.position.y, transform.position.z);
-           DD.transform.GetChild(0).SetParent(_chr.transform);
-           yield return new WaitForSeconds(0.35f);
-        }
-        yield break;
-    }
 
     private void Update()
     {
+      
         zoomInOut();
         avoidObstacle();
-       
-        if (Input.GetMouseButton(1) )
+
+        if (Input.GetMouseButtonDown(1))
         {
-            freeLook(); _isLinearTr = false; 
-           
+            GetSphereCoord(pos);
+            Elevation = transform.eulerAngles.x * Mathf.Deg2Rad;
         }
+
+        if (Input.GetMouseButton(1))
+        {
+            _isLinearTr = false;
+            SphereCoord();
+            
+        }
+       
         else
         {
-            vertMove = 15f; 
-         horMove = -90f * Mathf.Deg2Rad;
-         _isLinearTr = true;
-         pos = _chr.transform.position - transform.forward * camDistance;
-         rot = new Vector3(15f, 
-             _chr.transform.rotation.eulerAngles.y, 0);
-         pos.y += 1f;
+            _isLinearTr = true;
+            pos = _chr.transform.position - transform.forward * camDistance;
+            rot = new Vector3(transform.eulerAngles.x,
+                _chr.transform.rotation.eulerAngles.y, 0);
         }
+        pos.y += 1f;
        
     }
    
@@ -77,20 +104,29 @@ public class ThirdCam : MonoBehaviour
     void avoidObstacle()  // 레이캐스팅 + 장애물 피하기
     {
         RaycastHit hit;
-        Vector3 dir = transform.position - _chr.transform.position;
-       //Debug.DrawRay(_chr.transform.position, dir.normalized * dir.magnitude, Color.red);
-        if (Physics.Raycast(_chr.transform.position, dir.normalized, out hit, dir.magnitude, LayerMask.GetMask("Wall")))
+        Vector3 dir = _chr.transform.position- transform.position;
+        
+        // 카메라 앞에 장애물 있다 판정시
+        if (Physics.Raycast(transform.position, dir.normalized, out hit, dir.magnitude, LayerMask.GetMask("Wall")))
         {
-            Debug.Log(hit.transform.name);
             Vector3 dist = hit.point - _chr.transform.position;
-            camDistance = (dist.magnitude * _obstacleDist);
+            _obs = hit.point;                                      //0.95곱으로 살짝 틈을 줘야함, 안줄시 인식힘들어짐
+            camDistance = Mathf.Clamp(camDistance + (_camSpeed * Time.deltaTime), 1f, dist.magnitude*0.95f );
+            return;
         }
-        else
-        {
+    
+         if ((_obs - transform.position).magnitude < 1f)
+         {
+            return; 
+         }
+         
+         else
+         {
             camDistance = Mathf.Clamp(camDistance + (_camSpeed * Time.deltaTime), 1f, _maxDistance);
-        }
+         }
+            
     }
-
+ 
     void zoomInOut() // 마우스휠로 돌리었을떄 줌인 줌아웃
     {
         if (Input.GetAxis("Mouse ScrollWheel") == 0) { return; } // 스크롤입력없음 빠짐
@@ -98,61 +134,67 @@ public class ThirdCam : MonoBehaviour
         float scroll = -Input.GetAxis("Mouse ScrollWheel") * _scrollSpeed;
         _cam.fieldOfView = Mathf.Clamp(_cam.fieldOfView + scroll, 33f, 90f);
     }
-    void freeLook() // 휠클릭+마우스회전으로 자유시점
+    void freeLook() // SphereCoord()+GetCartesianCoord+GetSphereCoord 로 대체됨
     {
+        // 변수
+        // float horMove, vertMove;
+        // start에 작성
+        // horMove = -90f* Mathf.Deg2Rad;
+        // Update 에 작성 
+        // if => freeLook();
+        // else => vertMove = 15f; 
+        // else => horMove = -90f * Mathf.Deg2Rad;
+
+
+        // 휠클릭+마우스회전으로 자유시점
         // 원의 특징은, 반지름길이(R)만큼의 (X: (Cosθ)×(R) ,Y : (Sinθ)×(R) ) 좌표를 지닌 점들로 이루어진 모양(구체모형)
         // R반지름은 캐릭터와 카메라사이의 거리
         Vector3 center = _chr.transform.position; 
         Vector2 input = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        horMove += input.x * 100f* _MouseSensitivity.x * Time.deltaTime * Mathf.Deg2Rad ;
-        vertMove += input.y * 100f* _MouseSensitivity.y * Time.deltaTime ;
-        vertMove = Mathf.Clamp(vertMove, 0f, 90f);
-        float angle = (90.0f - vertMove) * Mathf.Deg2Rad;
+     // horMove += input.x * 100f* _mouseSensi * Time.deltaTime * Mathf.Deg2Rad ;
+     // vertMove += input.y * 100f* _mouseSensi * Time.deltaTime ;
+     // vertMove = Mathf.Clamp(vertMove, 0f, 90f);
+     //   float angle = (90.0f - vertMove) * Mathf.Deg2Rad;
        
-        center.y += camDistance * Mathf.Cos(angle);
-        center.x += camDistance * Mathf.Cos(horMove) * Mathf.Sin(angle);
-        center.z += camDistance * Mathf.Sin(horMove) * Mathf.Sin(angle);
+     //   center.y += camDistance * Mathf.Cos(angle);
+     //   center.x += camDistance * Mathf.Cos(horMove) * Mathf.Sin(angle);
+     //   center.z += camDistance * Mathf.Sin(horMove) * Mathf.Sin(angle);
     
-        pos = center;
-        rot = new Vector3(90f -angle*Mathf.Rad2Deg  , -horMove * Mathf.Rad2Deg - 90f, 0);
+     //   pos = center;
+     //   rot = new Vector3(90f -angle*Mathf.Rad2Deg  , -horMove * Mathf.Rad2Deg - 90f, 0);
     
-    }
-    Vector3 ToSphereCoordinate(Vector3 pos, Vector3 center, float radius = 0)
+    } 
+
+    public void SphereCoord()
     {
-        // 직교좌표계공간을 구면좌표계로 전환하는 함수
-        // 인자로 현재벡터값, 기준이나 원점이될 center
-        // 혹시 반지름이나 역할이 따로있다면 radius파라미터 채우면됨, 아니면 냅두길
-        // 구면좌표계 R θ Φ = 반지름,방위각,양각 (Radius,azimuth,elevation)
-        // 직교좌표계에서 구면좌표계로 
-        // (x y z) -> (R θ Φ)
-        // 반지름 R : Square(x^2+y^2+z^2)
-        // 방위각azimuth θ : Mathf.Acos(pos.z/radius) == Mathf.Atan2( Square(x^2+y^2) ,pos.z) 
-        // == Mathf.Atan2(pos.z, pos.x) 
-        // 양각elevation Φ : Mathf.Atan2(y,x) ==  Mathf.Asin(pos.y/radius)
-
-
-        Vector3 dir = pos ;
-        if (radius == 0) { radius = pos.magnitude; }
-
-        float azimuth = Mathf.Atan2(pos.z, pos.x) + Input.GetAxis("Mouse X") * _MouseSensitivity.x;
-        float elevation = Mathf.Asin(pos.y / radius) + Input.GetAxis("Mouse Y") * _MouseSensitivity.y;
-
-        return new Vector3(radius, azimuth * Mathf.Deg2Rad, elevation * Mathf.Deg2Rad);
+        Azimuth += Input.GetAxisRaw("Mouse X") * Time.deltaTime * _mouseSensi;
+        Elevation += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * _mouseSensi;
+        if (Mathf.Abs(Azimuth) >= Mathf.PI * 2)
+        { Azimuth = (Azimuth >= 0) ? -360f : +360f; }
+        rot = new Vector3(Elevation * Mathf.Rad2Deg,
+              -Azimuth * Mathf.Rad2Deg - 90f, 0);
+        pos = GetCartesianCoord() + _chr.transform.position;
     }
-    Vector3 ToCartesianCoordinate(float radius, float theta, float gamma)
+    public void GetSphereCoord(Vector3 cartesianCoord)
     {
-        // 구면좌표계공간을 직교좌표계로 전환하는 함수
-        // 인자로 구면좌표계를 이루고있는 반지름, 방위각θ(azimuth)각도, 양각Φ(elevation)각도를 받음
-        // x = R * sinθ * cosΦ
-        // y = R * sinθ * sinΦ
-        // z = R * cosθ
+        Vector3 dir = cartesianCoord;
 
-        float x = radius * Mathf.Cos(gamma) * Mathf.Cos(theta);
-        float z = radius * Mathf.Sin(theta) * Mathf.Cos(gamma);
-        float y = radius * Mathf.Sin(gamma);
-        return new Vector3(x,y,z);
+        Radius = dir.magnitude; // 오리지날 => dir.magnitude;
+        Azimuth = Mathf.Atan2(dir.z, dir.x) ;
+        Elevation = Mathf.Asin(dir.y / dir.magnitude);  // 오리지날 => Mathf.Asin(dir.y / radius);
+       
     }
-   
+    
+    Vector3 GetCartesianCoord()
+    {
+        //float t = Radius * Mathf.Cos(Elevation);
+        return new Vector3
+            (
+             camDistance * Mathf.Cos(Elevation) * Mathf.Cos(Azimuth), // x
+             camDistance * Mathf.Sin(Elevation),  // y
+             camDistance * Mathf.Cos(Elevation) * Mathf.Sin(Azimuth)//z
+            );
+    }
     private void OnDrawGizmos()
     { 
         //Debug.DrawRay(transform.position, forwardDir * 7f, Color.red);
